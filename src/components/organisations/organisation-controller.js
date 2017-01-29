2ebd2ed2151428da27fdb6a3d071e93379b020bb
Daniel Brown <d.t.brown@outlook.com>
2017-01-29 23:19:22 +0000

    Added a controller (built using async/await!) for the Organisation model type. Doesn't work yet as the Organisation Model hasn't been added to csblogs-core

---
+++ src/components/organisations/organisation-controller.js
@@ -0,0 +1,31 @@
+import { Organisation } from 'csblogs-common';
+import log from '../../log';
+
+const PUBLIC_API_ATTRIBUTES = [
+  'id',
+  'name',
+  'link',
+  'imageURI',
+  'description'
+];
+
+const DEFAULT_ORDER = [
+  ['name', 'DESC']
+];
+
+export async function getAll() {
+  try {
+    return await Organisation.findAll({
+      attributes: PUBLIC_API_ATTRIBUTES,
+      order: DEFAULT_ORDER,
+      raw: true
+    });
+  } catch (error) {
+    log.error({ error }, 'Error getting list of all Organisations');
+    throw error;
+  }
+}
+
+export async function getById(id) {
+  throw new Error(`Not Implemented getById(). Won't find ${id}`);
+}
